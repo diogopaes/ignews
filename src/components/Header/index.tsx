f0f0607358363e845bdb4dc2f415717a27cb635b
Diogo Paes <>
2021-06-03 14:38:37 -0300

    Initied project and firsts components created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import { SignInButton } from '../SignInButton';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav >
+                    <Link href="/">
+                        <a className={styles.active}>Home</a>
+                    </Link>
+                    <Link href="/posts">
+                        <a>Posts</a>
+                    </Link>
+                </nav>
+                <SignInButton/>
+            </div>
+        </header>
+    )
+}
---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,8 @@
+
+import styles from './styles.module.scss'
+
+export function SignInButton(){
+    return(
+        <button type="button">Sign in with Github</button>
+    )
+}
