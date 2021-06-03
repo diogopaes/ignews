164879a2cfe90a9a5c2743c2b02fef4362cdc543
Diogo Paes <>
2021-06-03 18:05:41 -0300

    config stripe and show price product in to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetServerSideProps } from 'next';
+
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from '../styles/home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -20,7 +30,7 @@ export default function Home() {
 
           <p>
             Get acess to all the publications<br/>
-            <span>for $9,90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
           <SubscribeButton/>
@@ -32,4 +42,24 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IyMvMHfX9cjwsPxNoADL5Vo', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return{
+    props: {
+      product
+    }
+  }
 }
