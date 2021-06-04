95375850535d5f3a15804516508d09151b2bcb51
Diogo Paes <>
2021-06-03 22:37:55 -0300

    finished signIn and signOut google

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,29 @@
-import { FaGithub } from 'react-icons/fa';
+import { FaGoogle } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss'
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.btn}>
-            <FaGithub color="#04d361"/>
-            Diego
+    return session ? (
+        <button 
+            type="button" 
+            className={styles.btn}
+            onClick={() => signOut()}
+        >
+            <FaGoogle color="#04d361"/>
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ): (
-        <button type="button" className={styles.btn}>
-            <FaGithub color="#eba417"/>
+        <button 
+            type="button" 
+            className={styles.btn}
+            onClick={() => signIn('google')}
+        >
+            <FaGoogle color="#eba417"/>
             Sign in
         </button>
     );
